673072c386cb187423bf39bff3b2671283ba2ecb
Anja Ishmukhametova <miukki@gmail.com>
2018-04-26 11:39:40 +0800

    second commit

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ yanr build
 node test.js
 ```
 
+## Sources
+About bit converter source : https://msdn.microsoft.com/en-us/library/system.bitconverter(v=vs.110).aspx
+Source about Buffer https://nodejs.org/api/buffer.html#buffer_new_buffer_array
--- src/index.js
+++ src/index.js
@@ -12,22 +12,33 @@ type Config = {
   transaction: string
 }
 
-// prettier-ignore
+type TxInputOutput = {
+  Address: string,
+  Currency: string,
+  Amount: string
+}
 
 const ERRORS = {
   ERROR: new Error('Custome Error')
 }
 
-type BUFFERS_TYPE = {
+// prettier-ignore
+type Buffers = {
   EXPIRE: Buffer,
   FEES: Buffer,
-  DECLARATIONS: Buffer
+  DECLARATIONS: Buffer,
+  INPUTS: Buffer,
+  OUTPUTS: Buffer,
+  MESSAGE: Buffer,
 }
 
-const BUFFERS: BUFFERS_TYPE = {
+const BUFFERS: Buffers = {
   EXPIRE: new Buffer(8),
   FEES: new Buffer(1),
-  DECLARATIONS: new Buffer(1)
+  DECLARATIONS: new Buffer(1),
+  INPUTS: new Buffer(1),
+  OUTPUTS: new Buffer(1),
+  MESSAGE: new Buffer(1)
 }
 
 export default class CryptographyTask {
@@ -49,10 +60,12 @@ export default class CryptographyTask {
     this.transaction = JSON.parse(Config.transaction)
   }
 
-  parseDeclarations() {
-    this.buffers.DECLARATIONS.writeUInt8(0)
-  }
-  
+  //Write(transaction.Expire)
+  // protected void Write(long longData)
+  // {
+  //     stream.Write(BitConverter.GetBytes(longData), 0, 8);
+  // }
+  //it is meaning 8 bytes  put to Buffer Array
   parseExpire() {
     const expire = new BigNumber(this.transaction.Expire + '')
     const expireLow = parseInt(expire.toString(16).slice(-8), 16)
@@ -63,6 +76,22 @@ export default class CryptographyTask {
     this.buffers.EXPIRE.writeUInt32LE(expireHigh, 4)
   }
 
+  //WriteNullable(transaction.Fees, Write)
+  // private void WriteNullable<T>(T data, Action<T> write)
+  // {
+  //     if (data == null)
+  //         Write(false);
+  //     else
+  //     {
+  //         Write(true);
+  //         write(data);
+  //     }
+  // }
+  // private void Write(bool value)
+  // {
+  //     stream.Write(BitConverter.GetBytes(value), 0, 1);
+  // }
+
   parseFees() {
     if (this.transaction.Fees == null) {
       this.buffers.FEES = new Buffer(1)
@@ -72,6 +101,70 @@ export default class CryptographyTask {
       this.buffers.FEES.writeUInt8(1, 0)
       this.buffers.FEES.writeDoubleLE(this.transaction.Fees, 1)
     }
+  }
+
+  //just write 1 byte to 0. Idea is if something null or empt dont put it in the json
+  parseDeclarations() {
+    this.buffers.DECLARATIONS.writeUInt8(0, 0)
+  }
+
+  parseInputs() {
+    this.buffers.INPUTS = CryptographyTask.txios2Buffer(this.transaction.Inputs)
+  }
+
+  parseOutputs() {
+    this.buffers.OUTPUTS = CryptographyTask.txios2Buffer(
+      this.transaction.Outputs
+    )
+  }
+
+  static txios2Buffer(txios: Array<TxInputOutput>) {
+    const countBuffer = new Buffer(1)
+    countBuffer.writeUInt8(txios.length, 0)
+    const txiosBuffer = Buffer.concat(
+      txios.map(txio => CryptographyTask.txio2Buffer(txio))
+    )
+    return Buffer.concat([countBuffer, txiosBuffer])
+  }
+
+  static txio2Buffer(txio: TxInputOutput) {
+    //  addressBuffer unfinished is address, as described the address is encoded in bech32 format.
+    //try to decode it. Nodejs bech32 library tips address lacking separate symbol
+    const addressBuffer = new Buffer(21) // TODO
+
+    const currencyBuffer = CryptographyTask.currency2Buffer(txio.Currency)
+    const amountBuffer = new Buffer(8)
+    amountBuffer.writeDoubleLE(+txio.Amount, 0)
+    return Buffer.concat([addressBuffer, currencyBuffer, amountBuffer])
+  }
+
+  static currency2Buffer(currency: string) {
+    let sum = 0
+    let multiplier = 1
+    for (let i = 0; i < 3; i++) {
+      sum += currency.charCodeAt(2 - i) * multiplier
+      multiplier *= 26
+    }
+    const currencyBuffer = new Buffer(2)
+    currencyBuffer.writeUInt16LE(sum, 0)
+    return currencyBuffer
+  }
+
+  parseMessage() {
+    //transactionMessage is exactly 10 bytes, that's also the reason we can see 10 \u0000 in message
+    //Payment.cs line 179
+    this.buffers.MESSAGE.writeUInt8(0, 0)
+  }
+
+  toBase64(): string {
+    return Buffer.concat([
+      this.buffers.EXPIRE,
+      this.buffers.FEES,
+      this.buffers.DECLARATIONS,
+      this.buffers.INPUTS,
+      this.buffers.OUTPUTS,
+      this.buffers.MESSAGE
+    ]).toString('base64')
   }
 
   methodTest(arg: string): Object {
